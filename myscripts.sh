317045699505e088e83ac4796b097e0ceac75d4e
Nathaniel Thomas <nxt220001@sysbio.utdallas.edu>
2023-06-08 11:15:35 -0500

    test

---
+++ myscripts.sh
@@ -0,0 +1,7 @@
+total 4
+drwxr-xr-x 2 nxt220001 applied-genomics  10 Jun  6 11:05 foo
+-rw-r--r-- 1 nxt220001 applied-genomics   0 Jun  6 11:29 myscripts.sh
+-rw-r--r-- 1 nxt220001 applied-genomics 133 Jun  6 11:10 testfile
+set -uex
+
+testfile
