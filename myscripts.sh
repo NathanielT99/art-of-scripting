9d2c24a1d8d73e2dc61b0d21e29387e4a4f1d385
Nathaniel Thomas <nxt220001@sysbio.utdallas.edu>
2023-06-12 11:41:10 -0500

    t

--- myscripts.sh
+++ myscripts.sh
@@ -3,5 +3,6 @@ drwxr-xr-x 2 nxt220001 applied-genomics  10 Jun  6 11:05 foo
 -rw-r--r-- 1 nxt220001 applied-genomics   0 Jun  6 11:29 myscripts.sh
 -rw-r--r-- 1 nxt220001 applied-genomics 133 Jun  6 11:10 testfile
 set -uex
+grep -o -w 'gene' SGD_features.tab | wc -l
 
 testfile
