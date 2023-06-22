6ae5c56d6b5710e5d595dcb719ed39ae9b6fefd1
Nathaniel Thomas <nxt220001@sysbio.utdallas.edu>
2023-06-22 11:27:10 -0500

    commitafterignoringothers

---
+++ homework3.sh
@@ -0,0 +1,10 @@
+#Question 7- Remove the # to see the output
+#NAME=$1
+#echo $2, $3, $NAME
+#Run with bash homework3.sh Hello World
+
+#Question 8
+NAME=$1
+echo '$NAME' "$NAME" $NAME
+#Run with bash homework3.sh JO
+
--- testinstall.R
+++ testinstall.R
@@ -1,6 +1,9 @@
 #Run deseq2
-library("Deseq2")
+#library("Deseq2")
 
-countData <- read.table("countData.txt"', header = TRUE, row.names = 1)
-dds <- DESeqDataSetFromMatrix(countData = countData, colData = colData, design = ~ condition)   
+#countData <- read.table("countData.txt"', header = TRUE, row.names = 1)
+#dds <- DESeqDataSetFromMatrix(countData = countData, colData = colData, design = ~ condition)   
 
+print("Hello World!")
+
+testingthing = newthing
